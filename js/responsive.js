2ef2c4bd00fc2699599edc47c346f2d366b40a42
Puneet489 <puneetbhardwaj183@gmail.com>
2022-06-18 16:09:32 +0530

    responsive website error correct and basic contact page added

--- js/responsive.js
+++ js/responsive.js
@@ -7,6 +7,5 @@ burger.addEventListener('click', () => {
 
     rightNav.classList.toggle('v-class');
     navList.classList.toggle('v-class');
-    navBar.classList.toggle('h-class');
-    document.getElementsByClassName('navbar')[0].style.height = "461px";
+    navBar.classList.toggle('h-nav');
 })
