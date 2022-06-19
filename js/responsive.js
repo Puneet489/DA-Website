7ac100ba076371d13aae7c39fdcaea157eb7abf2
Puneet489 <puneetbhardwaj183@gmail.com>
2022-06-20 00:29:21 +0530

    donate page created and paypal linked with button changing input

--- js/responsive.js
+++ js/responsive.js
@@ -9,3 +9,18 @@ burger.addEventListener('click', () => {
     navList.classList.toggle('v-class');
     navBar.classList.toggle('h-nav');
 })
+
+function change500() {
+    var txt = 500;
+    document.getElementById("amount").value = txt;
+}
+
+function change1000() {
+    var txt = 1000;
+    document.getElementById("amount").value = txt;
+}
+
+function change10000() {
+    var txt = 10000;
+    document.getElementById("amount").value = txt;
+}
